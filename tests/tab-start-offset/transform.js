bdbcfb33b9cc5e96496bf02573a4f83d5e0164eb
Samy Pesse <samypesse@gmail.com>
2016-08-07 23:47:23 +0200

    Add unit test for tab, indent multiple lines when pressing tab

---
+++ tests/tab-start-offset/transform.js
@@ -0,0 +1,18 @@
+
+module.exports = function(plugin, state) {
+    let block = state.document.findDescendant(node => node.type == 'code_block');
+
+    let withCursor = state.transform()
+        .collapseToStartOf(block)
+        .moveToOffsets(0)
+        .apply();
+
+    return plugin.onKeyDown(
+        {
+            preventDefault: function() {},
+            stopPropagation: function() {}
+        },
+        { key: 'tab' },
+        withCursor
+    );
+};
