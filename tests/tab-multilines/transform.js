a73f53af0a87e15432cbcbf64c589bd0c354d666
Soreine <soreine.plume@gmail.com>
2016-12-13 12:38:06 +0100

    Adapt tests to new structure

--- tests/tab-multilines/transform.js
+++ tests/tab-multilines/transform.js
@@ -1,18 +1,11 @@
 
 module.exports = function(plugin, state) {
-    const block = state.document.findDescendant(node => node.type == 'code_block');
-
-    const withCursor = state.transform()
-        .collapseToStartOf(block)
-        .moveToOffsets(0, 9)
-        .apply();
-
     return plugin.onKeyDown(
         {
             preventDefault() {},
             stopPropagation() {}
         },
         { key: 'tab' },
-        withCursor
+        state
     );
 };
