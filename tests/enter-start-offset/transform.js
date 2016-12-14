f9a319f044f15514b6cd03c25284c415754a4c95
Soreine <soreine.plume@gmail.com>
2016-12-14 12:18:18 +0100

    Adapt onEnter

--- tests/enter-start-offset/transform.js
+++ tests/enter-start-offset/transform.js
@@ -1,18 +1,11 @@
 
 module.exports = function(plugin, state) {
-    const block = state.document.findDescendant(node => node.type == 'code_block');
-
-    const withCursor = state.transform()
-        .collapseToStartOf(block)
-        .moveToOffsets(0)
-        .apply();
-
     return plugin.onKeyDown(
         {
             preventDefault() {},
             stopPropagation() {}
         },
         { key: 'enter' },
-        withCursor
+        state
     );
 };
