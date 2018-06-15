e5452c51b921187b77f96da59a46d924ec810f58
Soreine <soreine.plume@gmail.com>
2018-06-15 18:26:41 +0200

    Improve normalization by wrapping orphan lines together

---
+++ tests/schema-no-multiple-orphan-lines/expected.js
@@ -0,0 +1,17 @@
+/** @jsx hyperscript */
+import hyperscript from '../hyperscript';
+
+export default (
+    <value>
+        <document>
+            <code_block>
+                <code_line>Line 1</code_line>
+                <code_line>Line 2</code_line>
+            </code_block>
+            <paragraph>cut</paragraph>
+            <code_block>
+                <code_line>Line 3</code_line>
+            </code_block>
+        </document>
+    </value>
+);
---
+++ tests/schema-no-multiple-orphan-lines/input.js
@@ -0,0 +1,13 @@
+/** @jsx hyperscript */
+import hyperscript from '../hyperscript';
+
+export default (
+    <value>
+        <document>
+            <code_line>Line 1</code_line>
+            <code_line>Line 2</code_line>
+            <paragraph>cut</paragraph>
+            <code_line>Line 3</code_line>
+        </document>
+    </value>
+);
