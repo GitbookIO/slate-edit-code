c5d03f2d3c33781fbea608230f5606f814b93b82
Soreine <soreine.plume@gmail.com>
2018-06-19 12:20:52 +0200

    Add failing test

---
+++ tests/schema-only-lines/input.js
@@ -0,0 +1,14 @@
+/** @jsx hyperscript */
+import hyperscript from '../hyperscript';
+
+export default (
+    <value>
+        <document>
+            <code_block>
+                <code_line>Hello</code_line>
+                <paragraph>invalid</paragraph>
+                <code_line>World</code_line>
+            </code_block>
+        </document>
+    </value>
+);
