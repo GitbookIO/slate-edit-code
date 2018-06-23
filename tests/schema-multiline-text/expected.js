bc8401d5cfdd73f80c12f7e9057e9fdfda358d14
Soreine <soreine.plume@gmail.com>
2018-06-23 20:14:02 +0200

    Lint

--- tests/schema-multiline-text/expected.js
+++ tests/schema-multiline-text/expected.js
@@ -1,15 +1,15 @@
 /** @jsx hyperscript */
-import hyperscript from "../hyperscript";
+import hyperscript from '../hyperscript';
 
 export default (
-  <value>
-    <document>
-      <code_block>
-        <code_line>This node must be converted.multiple</code_line>
-        <code_line>lines</code_line>
-        <code_line>of</code_line>
-        <code_line>code and marks</code_line>
-      </code_block>
-    </document>
-  </value>
+    <value>
+        <document>
+            <code_block>
+                <code_line>This node must be converted.multiple</code_line>
+                <code_line>lines</code_line>
+                <code_line>of</code_line>
+                <code_line>code and marks</code_line>
+            </code_block>
+        </document>
+    </value>
 );
